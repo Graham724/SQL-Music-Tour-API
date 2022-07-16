46e5f79cbc6ebad38f5e1f6bb484e3adca14561c
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-07-15 19:39:22 -0500

    added models to database

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 // DEPENDENCIES
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+// // SEQUELIZE CONNECTION
+// const sequelize = new Sequelize({
+//     storage: process.env.PG_URI,
+//     dialect: 'postgres',
+//     username: 'postgres',
+//     password: process.env.PASSWORD
+//   })
+  
+
+// try {
+//     sequelize.authenticate() 
+//     console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+// } catch(err) {
+//     console.log(`Unable to connect to PG: ${err}`) 
+// }
+
 
 // CONFIGURATION / MIDDLEWARE
-require('dotenv').config()
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
